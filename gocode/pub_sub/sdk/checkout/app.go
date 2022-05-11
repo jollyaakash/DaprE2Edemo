1afd97f1cf20e33fcf46a594fbba76589b06d2b2
Aakash Jolly <aajolly@microsoft.com>
2022-05-11 10:11:30 -0700

    Update

--- gocode/pub_sub/sdk/checkout/app.go
+++ gocode/pub_sub/sdk/checkout/app.go
@@ -3,10 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
+	"net"
 	"strconv"
 	"time"
 
-	dapr "github.com/dapr/go-sdk/client"
+	"github.com/spiffe/go-spiffe/v2/spiffeid"
+	"github.com/spiffe/go-spiffe/v2/svid/jwtsvid"
+	"github.com/spiffe/go-spiffe/v2/workloadapi"
 )
 
 var (
@@ -14,23 +17,70 @@ var (
 	PUBSUB_TOPIC = "orders"
 )
 
-func main() {
-	client, err := dapr.NewClient()
+func testSpiffe(addr string) {
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	clientOptions := workloadapi.WithClientOptions(workloadapi.WithAddr(addr))
+
+	fmt.Printf("CLIENT OPTIONS")
+
+	jwtSource, err := workloadapi.NewJWTSource(
+		ctx,
+		clientOptions,
+	)
+
+	if err != nil {
+		fmt.Printf("PANIC FROM NewJWTSource")
+		panic(err)
+	} else {
+		fmt.Println("successfully got SPIFFE ID")
+	}
+
+	defer jwtSource.Close()
+
+	serverID := spiffeid.RequireFromString("spiffe://iotedge/mqttbroker")
+	audience := serverID.String()
+
+	svid, err := jwtSource.FetchJWTSVID(ctx, jwtsvid.Params{
+		Audience: audience,
+	})
+
 	if err != nil {
 		panic(err)
 	}
-	defer client.Close()
-	ctx := context.Background()
+
+	fmt.Printf("message bus got SPIFFE ID - %s", svid.Marshal())
+}
+
+func main() {
+	listener, err := net.Dial("unix", "/run/iotedge/sockets/workloadapi.sock")
+	if err != nil {
+		panic(err)
+	}
+
+	addr := fmt.Sprintf("%s://%s", listener.RemoteAddr().Network(), listener.RemoteAddr().String())
+	fmt.Printf("%s", addr)
+
+	testSpiffe(addr)
+
+	// client, err := dapr.NewClient()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer client.Close()
+	// ctx := context.Background()
+
 	for i := 1; i <= 2000; i++ {
 		order := `{"orderId":` + strconv.Itoa(i) + `}`
 
-		// Publish an event using Dapr pub/sub
-		if err := client.PublishEvent(ctx, PUBSUB_NAME, PUBSUB_TOPIC, []byte(order)); err != nil {
-			panic(err)
-		}
+		// // Publish an event using Dapr pub/sub
+		// if err := client.PublishEvent(ctx, PUBSUB_NAME, PUBSUB_TOPIC, []byte(order)); err != nil {
+		// 	panic(err)
+		// }
 
 		fmt.Println("Published data: ", order)
 
-		time.Sleep(2*time.Second)
+		time.Sleep(2 * time.Second)
 	}
 }
---
+++ gocode/pub_sub/sdk/checkout/rust/spiffee/src/main.rs
@@ -0,0 +1,17 @@
+use spiffe::workload_api::client::WorkloadApiClient;
+use spiffe::spiffe_id::{SpiffeId};
+
+fn main() {
+    let client = WorkloadApiClient::new("unix:/run/iotedge/sockets/workloadapi.sock").unwrap();
+    let spiffe_id = SpiffeId::try_from("spiffe://iotedge/my-service").unwrap();
+
+    let bundle = client.fetch_jwt_bundles().unwrap();
+
+    bundle.get_bundle(trust_domain)
+
+    let target_audience = &["spiffe://iotedge/mqttbroker"];
+    // fetch a jwt token for the provided SPIFFE-ID and with the target audience `service1.com`
+    let jwt_svid = client.fetch_jwt_svid(target_audience, Some(&spiffe_id)).unwrap();
+    let test = jwt_svid.spiffe_id().path().clone();
+    println!("{}", test.to_string());
+}
